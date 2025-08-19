422beca7984e5657cf07ddc499d17e8ecb552297
Krzysztof Furman <k_furman@outlook.com>
2025-08-19 17:47:05 +0100

    Fixed redundant line.

--- functions.hpp
+++ functions.hpp
@@ -1,8 +1,6 @@
 #ifndef CPPSYNTAXREVISION_FUNCTIONS_H
 #define  CPPSYNTAXREVISION_FUNCTIONS_H
 int addition (int a, int b); //ended with a semicolon ; yuo only declare that the function exist
-// A regular function declaration
-int addition(int a, int b);
 
 // --- Template function DEFINITION ---
 // The entire template, including the body, goes in the header file.
